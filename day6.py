10d901787b3e6656d8698bcf9baa01c0af4fd4ec
VahinL <vahinlallp@gmail.com>
2024-04-30 10:00:22 +0530

    second commit

---
+++ day6.py
@@ -0,0 +1,52 @@
+###GETATTR
+# class Person:
+#     name = "John"
+#     age = 36
+#     country = "Norway"
+# x = getattr(Person,'age')
+# print(x)
+# x = getattr(Person,'age1','not present')
+# print(x)
+
+##DELATTR
+# class Person:
+#     name = "John"
+#     age = 36
+#     country = "Norway"
+# delattr(Person.age)
+# print(x)
+
+###HASATTR
+# class Person:
+#     name = "John"
+#     age = 36
+#     country = "Norway"
+
+# x = hasattr(Person,'age')
+# print(x)
+
+##SETATTR
+# class Person:
+#     name = "John"
+#     age = 36
+#     country = "Norway"
+# x = getattr(Person,'age')
+# print(x)
+# setattr(Person,"age1",45)
+# x1 = getattr(Person,'age1')
+# print(x1)
+
+###ISSUBCLASS
+# class MyParent():
+#     age = 36
+
+# class myChild(MyParent):
+#     name = "John"
+#     age = 9
+
+# x = issubclass(myChild,MyParent)
+# print(x)
+
+# x = issubclass(MyParent,myChild)
+# print(x)
+
